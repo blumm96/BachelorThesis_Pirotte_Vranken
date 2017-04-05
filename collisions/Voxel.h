8b8ba9d6a3a95c0801dc61fce77f7873436e7cbd
cvranken <casper.vranken@student.uhasselt.be>
2017-04-05 17:07:08 +0200

    Doc Voxel

--- collisions/Voxel.h
+++ collisions/Voxel.h
@@ -1,39 +1,6 @@
 //==============================================================================
 /*
-Software License Agreement (BSD License)
-Copyright (c) 2003-2016, CHAI3D.
-(www.chai3d.org)
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following
-disclaimer in the documentation and/or other materials provided
-with the distribution.
-
-* Neither the name of CHAI3D nor the names of its contributors may
-be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+This class represents a Voxel.
 
 \author    Niels Pirotte
 \author    Casper Vranken
@@ -99,23 +66,56 @@ namespace chai3d {
 
 	public:
 		//getters
+		/*
+			
+			Get the position of the voxel.
+
+			\return The position of the voxel.
+
+		*/
 		inline cVector3d* getPos() { return pos; };
+
+		/*
+			
+			Get the minimum distance to the surface.
+
+		*/
 		inline float getMinDist() { return minDist; };
 
 		//setters
+		// Set the position of the voxel.
 		void setPos(const double &x, const double &y, const double &z);
+		// Set the minimum distance to the surface.
 		void setMinDist(float dist);
+
+		/*
+			
+			Set the triangle associated to the voxel.
+
+			\param setT The triangle to be set.
+
+		*/
 		inline void setTriangle(Triangle* setT) { t = setT; }
 
 	public:
 		//--------------------------------------------------------------------------
 		// MEMBERS:
 		//--------------------------------------------------------------------------
+
+		// The position of this voxel.
 		cVector3d* pos;
+		// The minimum distance to the surface.
 		float minDist;
+		// The triangle associated to this sphere.
 		Triangle* t;
 
 	public:
+
+		/*
+			
+			An operator used to sort the voxels.
+
+		*/
 		inline bool operator <(const Voxel* voxel1) {
 			return this->minDist > voxel1->minDist;
 		}
